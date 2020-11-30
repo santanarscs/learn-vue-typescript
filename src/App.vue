60e22be931c852e8039a328fc963f58d25332bbb
Raphael Santana <raphaelstn@gmail.com>
2020-11-30 15:56:24 -0300

    add calendar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <Header />
   <div class="w-full flex">
-    <DcHeros />
+    <Calendar />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "./components/Header.vue";
-import DcHeros from "./components/DcHeros.vue";
+// import DcHeros from "./components/DcHeros.vue";
+import Calendar from "./components/Calendar.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Header,
-    DcHeros
+    Calendar
   }
 });
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,9 @@
 <template>
-  <nav class="w-full text-white px-4 py-2 bg-gradient-to-r from-blue-800 to-blue-600">
-    <a>Dc Heros</a>
+  <nav class="w-full text-white px-4 py-4 bg-gradient-to-r from-blue-800 to-blue-600">
+    <a>Learn Vue</a>
   </nav>
 </template>
 
 <script>
 export default {};
 </script>
-
