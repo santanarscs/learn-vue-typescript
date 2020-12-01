bb06942d6650953845ee5f35c6667c2ecba51ca2
santanarscs <raphaelstn@gmail.com>
2020-12-01 14:39:45 -0300

    slider carousel

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,31 @@
 <template>
-  <nav class="w-full text-white px-4 py-4 bg-gradient-to-r from-blue-800 to-blue-600">
-    <router-link v-for="item of list" class="mx-2" :key="item.to" :to="item.to">{{item.title}}</router-link>
+  <nav
+    class="w-full text-white px-4 py-4 bg-gradient-to-r from-blue-800 to-blue-600"
+  >
+    <router-link
+      v-for="item of list"
+      class="mx-2"
+      :key="item.to"
+      :to="item.to"
+      >{{ item.title }}</router-link
+    >
   </nav>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
   data() {
-    return{
+    return {
       list: [
-      { title: 'Home', to: '/'},
-      { title: 'Dc Heros', to: '/dc-heros'},
-      { title: 'Calendar', to: '/calendar'},
-      { title: 'Markdown', to: '/markdown'},
-    ]
-    }
+        { title: "Home", to: "/" },
+        { title: "Dc Heros", to: "/dc-heros" },
+        { title: "Calendar", to: "/calendar" },
+        { title: "Markdown", to: "/markdown" },
+        { title: "Slider", to: "/slider" }
+      ]
+    };
   }
 });
 </script>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import DcHeros from '../pages/DcHeros.vue'
-import Calendar from '../pages/Calendar.vue'
-import Markdown from '../pages/Markdown.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import DcHeros from "../pages/DcHeros.vue";
+import Calendar from "../pages/Calendar.vue";
+import Markdown from "../pages/Markdown.vue";
+import Slider from "../pages/Slider.vue";
 const routes = [
-  { path: '/', component: Home},
-  { path: '/dc-heros', component: DcHeros},
-  { path: '/calendar', component: Calendar},
-  { path: '/markdown', component: Markdown}
-]
+  { path: "/", component: Home },
+  { path: "/dc-heros", component: DcHeros },
+  { path: "/calendar", component: Calendar },
+  { path: "/markdown", component: Markdown },
+  { path: "/slider", component: Slider }
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes
-})
+});
