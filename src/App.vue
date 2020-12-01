06935207e7bc3bf809273f0bbc96dfab4c88f02d
santanarscs <raphaelstn@gmail.com>
2020-12-01 16:51:35 -0300

    modal login

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <Header />
+  <Header @open-login-modal="isOpenLogin = true" />
   <div class="w-full flex">
     <router-view></router-view>
   </div>
+  <LoginModal v-if="isOpenLogin" @close-login="isOpenLogin = false" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "./components/Header.vue";
+import LoginModal from "./components/LoginModal.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    Header
+    Header,
+    LoginModal
+  },
+  data() {
+    return {
+      isOpenLogin: false
+    };
   }
 });
 </script>
---
+++ src/components/LoginModal.vue
@@ -0,0 +1,72 @@
+<template>
+  <section
+    @click="$emit('close-login')"
+    class="z-20 w-screen h-screen bg-gray-300 fixed top-0 opacity-50"
+  ></section>
+  <div class="absolute inset-0">
+    <div class="flex h-full">
+      <div class=" z-30 m-auto bg-white p-2 rounded shadow w-1/3">
+        <div class="p-2 border">
+          <h1 class="text-2xl text-center ">login</h1>
+          <form @click.prevent="submit" class="p-2 my-2">
+            <div class="my-4">
+              <label>E-mail or username</label>
+              <input
+                v-model="form.email"
+                class="rounded shadow p-2 w-full"
+                placeholder="Enter your email or username"
+              />
+            </div>
+            <div class="my-4">
+              <label>Password</label>
+              <input
+                v-model="form.password"
+                class="rounded shadow p-2 w-full"
+                type="password"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div class="my-4">
+              <button
+                class="
+                  w-full 
+                  text-white 
+                  rounded 
+                  shadow-md 
+                  bg-gradient-to-r 
+                  from-red-800
+                  p-2
+                  to-pink-600"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      console.log("alskjd");
+    }
+  }
+});
+</script>
+
+<style></style>
